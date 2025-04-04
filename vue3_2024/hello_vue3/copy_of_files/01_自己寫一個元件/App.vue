a119e7e0cb0a5f4747ee544f35c5be1645d1bf36
studyboy23 <ccps980115@gmail.com>
2025-04-04 13:05:00 +0800

    youtube vue3_007 2:32

---
+++ vue3_2024/hello_vue3/copy_of_files/01_自己寫一個元件/App.vue
@@ -0,0 +1,23 @@
+<template>
+    <!-- HTML -->
+     <div class="app">
+        <h1>你好啊!</h1>
+     </div>
+</template>
+    
+<script lang='ts'>
+    //JS or TS
+    export default {
+        name:"App" //元件名
+    }
+</script>
+    
+<style>
+    /* CSS */
+    .app {
+        background-color: #ddd;
+        box-shadow:0 0 10px;
+        border-radius: 10px;
+        padding: 20px;
+    }
+</style>
