a3d06ecf228270229225d860d815dda823481cbf
studyboy23 <ccps980115@gmail.com>
2025-06-26 20:47:26 +0800

    add hello_world project

---
+++ atguigu/HelloWorld/hello_world.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+
+using namespace std;
+
+void welcome() {
+	cout << "請輸入姓名：" << endl;
+	
+	string name;
+
+	cin >> name;
+
+	cout << "Hello，歡迎 " << name;
+}
+
+
+int main() {
+
+	cout << "Hello World!" << endl;
+
+	welcome();
+
+	cin.get();
+	cin.get();
+
+}
