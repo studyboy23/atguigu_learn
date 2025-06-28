2b7070587162983ea0d4cfa7dd120e9196f925af
studyboy23 <ccps980115@gmail.com>
2025-06-28 23:26:48 +0800

    test

--- atguigu/HelloWorld/hello_world.cpp
+++ atguigu/HelloWorld/hello_world.cpp
@@ -2,15 +2,7 @@
 
 using namespace std;
 
-void welcome() {
-	cout << "請輸入姓名：" << endl;
-	
-	string name;
 
-	cin >> name;
-
-	cout << "Hello，歡迎 " << name;
-}
 
 
 int main() {
---
+++ atguigu/HelloWorld/welcome.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+void welcome() {
+	cout << "請輸入姓名：" << endl;
+
+	string name;
+
+	cin >> name;
+
+	cout << "Hello，歡迎 " << name <<endl;
+}
